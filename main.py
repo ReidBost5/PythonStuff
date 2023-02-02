5f168a0c1e36288362a71632f85434f6020c4c2e
Reid Bost <reid.bost@gmail.com>
2023-02-02 03:04:43 -0500

    just random stuff until I figure out what I plan to do with this document

---
+++ main.py
@@ -0,0 +1,20 @@
+# I want to try to do something with mobile devices but not sure what I can do with it yet so this is just a random
+# document for now
+
+class Testing:
+
+    def __init__(self, username):
+        self.username = username
+
+    def return_user(self):
+        print(self.username)
+
+# Main method
+
+
+TO = Testing("Reebus")
+
+TO.return_user()
+
+
+
